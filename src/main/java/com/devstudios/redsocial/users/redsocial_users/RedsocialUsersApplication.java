8729c5da45b102f083bfaf74b72c0879f550b7b9
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-02 11:18:16 -0600

    First commit

---
+++ src/main/java/com/devstudios/redsocial/users/redsocial_users/RedsocialUsersApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.redsocial.users.redsocial_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedsocialUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedsocialUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/redsocial/users/redsocial_users/RedsocialUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.redsocial.users.redsocial_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedsocialUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
