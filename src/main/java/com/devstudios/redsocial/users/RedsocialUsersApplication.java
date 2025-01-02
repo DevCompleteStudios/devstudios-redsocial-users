790f46be9cc9690dfaaac7e479a9f2c065db5ee8
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-02 12:13:49 -0600

    Agregando conexión con eureka y mongo DB

--- src/main/java/com/devstudios/redsocial/users/RedsocialUsersApplication.java
+++ src/main/java/com/devstudios/redsocial/users/RedsocialUsersApplication.java
@@ -1,9 +1,12 @@
-package com.devstudios.redsocial.users.redsocial_users;
+package com.devstudios.redsocial.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RedsocialUsersApplication {
 
 	public static void main(String[] args) {
